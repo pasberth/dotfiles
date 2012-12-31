1a12822082ec9be774550f1ce176e802a382322f
pasberth <pasberth@gmail.com>
2012-12-31 15:29:16 +0900

    el-get に移行する〜

--- emacs/emacs.d/init/plugin.el
+++ emacs/emacs.d/init/plugin.el
@@ -1,4 +1,5 @@
 
+;; auto-complete.el
 (unless (locate-library "auto-complete")
   (el-get 'sync 'auto-complete))
 
@@ -6,3 +7,26 @@
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/dict")
 (require 'auto-complete-config)
 (ac-config-default)
+
+
+
+;; quickrun.el
+(unless (locate-library "quickrun")
+  (el-get 'sync 'quickrun))
+(require 'quickrun)
+
+;; anything.el
+(unless (locate-library "anything")
+  (el-get 'sync 'anything))
+(require 'anything)
+
+;; popwin.el
+(unless (locate-library "popwin")
+  (el-get 'sync 'popwin))
+(require 'popwin)
+
+;; markdown-mode.el
+;(require 'markdown-mode)
+;(require 'yaml-mode)
+
+;(require 'macra-mode)
