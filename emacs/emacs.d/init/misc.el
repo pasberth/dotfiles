dba8518e7a47a8c0ecc2684bb4071153a85746fb
pasberth <pasberth@gmail.com>
2013-01-01 17:56:56 +0900

    設定追加

--- emacs/emacs.d/init/misc.el
+++ emacs/emacs.d/init/misc.el
@@ -28,3 +28,6 @@
 
 ;; タブを使わず 4スペースにする
 (setq-default tab-width 4 indent-tabs-mode nil)
+
+
+(setq ruby-deep-indent-paren nil)
--- zsh/keybinding.zsh
+++ zsh/keybinding.zsh
@@ -2,7 +2,7 @@
 # Key bindings
 # ==========================================================
 
-autoload -U history-search-end
+# autoload -U history-search-end
 
 # http://d.hatena.ne.jp/kei_q/20110406/1302091565
 function alls () {
@@ -10,13 +10,36 @@ function alls () {
   if [[ -z "$BUFFER" ]]; then
     echo
     ls
+
+    # git ディレクトリなら status 表示
+    git status 2> /dev/null
   fi
+
+  return 0
 }
 
 zle -N alls
-zle -N history-beginning-search-backward-end history-search-end
-zle -N history-beginning-search-forward-end history-search-end
+bindkey '^m' alls
+
+function uncd () {
+  echo
+  pushd ..
+  zle reset-prompt
+}
+
+function recd () {
+  echo
+  popd
+  zle reset-prompt
+}
+
+zle -N uncd
+zle -N recd
+bindkey '^z' uncd
+bindkey 'Z' recd
+
+# zle -N history-beginning-search-backward-end history-search-end
+# zle -N history-beginning-search-forward-end history-search-end
 
 #bindkey '^R' history-incremental-pattern-search-forward
 #bindkey '^S' history-incremental-pattern-search-backward
-bindkey '^m' alls
