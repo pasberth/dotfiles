7433b277051324fa86645b6da167e684b7e00a6a
pasberth <pasberth@gmail.com>
2013-07-12 21:51:03 +0900

    Mr. Zsh の Pull Request をマージした

--- osx/10.8/bash_profile.sh
+++ osx/10.8/bash_profile.sh
@@ -7,4 +7,5 @@ export PATH=/usr/local/bin:$PATH
 
 export MAKEFILEPATH=/Applications/Xcode.app/Contents/Developer/Makefiles
 
+alias ghc='ghc-7.6.3 -L/usr/lib'
 alias cabal='cabal --ghc-options=-L/usr/lib'
